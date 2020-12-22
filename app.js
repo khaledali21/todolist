11e91f8546d7541ea085d96b7f5ebf984fa2788a
moriarty21 <khaled21.ali33@gmail.com>
2020-12-22 23:35:43 +0200

    added gitignore and some stuff

--- app.js
+++ app.js
@@ -139,7 +139,10 @@ app.get("/:listName", function(req,res){
 app.get("/about", function(req, res){
   res.render("about");
 });
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started succesfully");
 });
