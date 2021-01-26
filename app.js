4572dd72375f48fcac69fc9b2b2ae8a3e620242e
moriarty21 <>
2021-01-26 11:58:34 +0200

    Add list of items to choose instead of typing url

--- app.js
+++ app.js
@@ -40,42 +40,63 @@ const listSchema = new mongoose.Schema({
   items: [itemSchema]
 });
 const List = mongoose.model("list", listSchema);
-app.get("/", function(req, res) {
-  Item.find({}, function(err, items){
+// app.get("/", function(req, res) {
+//   Item.find({}, function(err, items){
+//     if(err){
+//       console.log(err);
+//     }
+//     else{
+//       if(items.length === 0){
+//         Item.insertMany(defaultItems, function(err){
+//           if(err){
+//             console.log(err);
+//           }
+//           else{
+//             res.redirect("/");
+//           }
+//         });
+//       }
+//       else{
+//         res.render("list", {listTitle: "Today", newListItems: items});
+//       }
+//     }
+//   });
+// });
+app.get("/", (req, res)=>{
+  List.find({}, (err, lists)=>{
     if(err){
       console.log(err);
     }
     else{
-      if(items.length === 0){
-        Item.insertMany(defaultItems, function(err){
-          if(err){
-            console.log(err);
-          }
-          else{
-            res.redirect("/");
-          }
+      res.render("home", {lists: lists});
+    }
+  })
+})
+app.post("/addList", function(req, res){
+  const listName = _.capitalize(req.body.newList);
+  List.findOne({name: listName}, function(err, foundList){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(!foundList){
+        const list = new List({
+          name: listName,
+          items: defaultItems
         });
+        list.save();
       }
-      else{
-        res.render("list", {listTitle: "Today", newListItems: items});
-      }
+      res.redirect("/");
     }
   });
 });
-
-app.post("/", function(req, res){
+app.post("/addItem", function(req, res){
 
   const itemName = req.body.newItem;
-
   const listName = req.body.list;
   const newItem = new Item({
     name: itemName
   });
-  if(listName === "Today"){
-    newItem.save();
-    res.redirect("/");
-  }
-  else{
     List.findOne({name: listName}, function(err, foundList){
       if(err){
         console.log(err);
@@ -83,17 +104,13 @@ app.post("/", function(req, res){
       else{
         foundList.items.push(newItem);
         foundList.save();
-        res.redirect("/" + listName);
+        res.redirect("/"+listName);
       }
     })
-  }
-
 });
-app.post("/delete", function(req, res){
-  const itemID = req.body.checkbox;
-  const listName = req.body.listName;
-  if(listName === "Today"){
-    Item.findOneAndDelete ({_id: itemID}, function(err){
+app.post("/deleteList", function(req, res){
+  const listID = req.body.checkbox;
+    List.findOneAndDelete ({_id: listID}, function(err){
       if(err){
         console.log(err);
       }
@@ -101,8 +118,10 @@ app.post("/delete", function(req, res){
          res.redirect("/");
       }
     });
-  }
-  else{
+  });
+app.post("/deleteItem", function(req, res){
+  const itemID = req.body.checkbox;
+  const listName = req.body.listName;
     List.findOneAndUpdate({name: listName},
       {$pull: {items: {_id: itemID}}},
       function(err, foundList){
@@ -113,9 +132,6 @@ app.post("/delete", function(req, res){
         res.redirect("/" + listName);
       }
     })
-  }
-
-
 });
 app.get("/:listName", function(req,res){
   const listName = _.capitalize(req.params.listName);
